988179fb239fad001e10e9f99336d3534f5e903d
Alexey Seren <dehaekx@gmail.com>
2024-05-19 15:40:31 +0700

    add Modifiers for c# java and redid methods compile for java

--- methodunit.cpp
+++ methodunit.cpp
@@ -11,15 +11,12 @@ std::string CPlusMethod::compile(unsigned int level) const
     {
         result += "virtual ";
     }
-
     result += m_returnType + " ";
     result += m_name + "()";
-
     if( m_flags & CONST )
     {
         result += " const";
     }
-
     result += " {\n";
     for( const auto& b : m_body )
     {
@@ -32,13 +29,21 @@ std::string CPlusMethod::compile(unsigned int level) const
 std::string JavaMethod::compile(unsigned int level) const // добавить нормальную реализацию
 {
     std::string result = generateShift( level );
-    if( m_flags & STATIC )
+    if( m_flags & STATIC && m_flags & FINAL )
+    {
+        result += "static final ";
+    }
+    else if( m_flags & STATIC )
     {
         result += "static ";
     }
-    else if( m_flags & VIRTUAL )
+    else if (m_flags & FINAL)
     {
-        result += "virtual ";
+        result += "final";
+    }
+    else if (m_flags & ABSTRACT)
+    {
+        result += "abstract";
     }
     result += m_returnType + " ";
     result += m_name + "()";
--- methodunit.h
+++ methodunit.h
@@ -2,22 +2,24 @@
 #define METHODUNIT_H
 #include "unit.h"
 #include "cassert"
-class MethodUnit : public Unit
+class AbstractMethodUnit : public Unit
 {
 public:
-    enum Modifier
+    enum Modifier // ???????????????
     {
         STATIC = 1,
         CONST = 1 << 1,
-        VIRTUAL = 1 << 2
+        VIRTUAL = 1 << 2,
+        FINAL = 1 << 3,
+        ABSTRACT = 1 << 4
     };
 public:
-    MethodUnit( const std::string& name, const std::string& returnType, Flags flags ) :
+    AbstractMethodUnit( const std::string& name, const std::string& returnType, Flags flags ) :
         m_name( name ), m_returnType( returnType ), m_flags( flags ) { }
 
     void add( const std::shared_ptr< Unit >& unit, Flags /* flags */ = 0 )
     {
-        assert(unit != NULL); // check for no nullprt
+       // assert(unit != NULL); // check for no nullprt
         m_body.push_back( unit );
     }
 
@@ -28,24 +30,27 @@ protected:
     std::vector< std::shared_ptr< Unit > > m_body;
 };
 
-class CPlusMethod: public MethodUnit
+class CPlusMethod: public AbstractMethodUnit
 {
+public:
     CPlusMethod( const std::string& name, const std::string& returnType, Flags flags ) :
-        MethodUnit(name, returnType, flags) {}
+        AbstractMethodUnit(name, returnType, flags) {}
     std::string compile( unsigned int level = 0) const;
 };
 
-class JavaMethod: public MethodUnit
+class JavaMethod: public AbstractMethodUnit
 {
+public:
     JavaMethod( const std::string& name, const std::string& returnType, Flags flags ) :
-        MethodUnit(name, returnType, flags) {}
+        AbstractMethodUnit(name, returnType, flags) {}
     std::string compile( unsigned int level = 0) const;
 };
 
-class CSharpMethod: public MethodUnit
+class CSharpMethod: public AbstractMethodUnit
 {
+public:
     CSharpMethod( const std::string& name, const std::string& returnType, Flags flags ) :
-        MethodUnit(name, returnType, flags) {}
+        AbstractMethodUnit(name, returnType, flags) {}
     std::string compile( unsigned int level = 0) const;
 };
 
