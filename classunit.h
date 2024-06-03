b73e3ef884d5527ba7e88ac47d4d2c951f5e763f
Alexey Seren <dehaekx@gmail.com>
2024-06-03 15:58:17 +0700

    moved classes into separate files

--- classunit.h
+++ classunit.h
@@ -26,46 +26,4 @@ protected:
     std::vector< Fields > m_fields; // поля класса, сгруппированные по модификаторам доступа
 };
 
-
-
-class CPlusCLass: public AbstractClassUnit
-{
-public:
-    const std::vector< std::string > ACCESS_MODIFIERS =
-        { "public", "protected", "private"};
-    CPlusCLass(const std::string& name):AbstractClassUnit(name)
-    {
-        m_fields.resize(ACCESS_MODIFIERS.size());
-    }
-    void add(const std::shared_ptr< Unit >& unit, Flags flags) override;
-    std::string compile( unsigned int level = 0, std::string access_modifiers = "" ) const override;
-};
-
-class JavaClass: public AbstractClassUnit
-{
-public:
-    const std::vector< std::string > ACCESS_MODIFIERS =
-        { "public", "protected", "private"};
-    JavaClass(const std::string& name):AbstractClassUnit(name)
-    {
-        m_fields.resize(ACCESS_MODIFIERS.size());
-    }
-    void add(const std::shared_ptr< Unit >& unit, Flags flags) override;
-    std::string compile( unsigned int level = 0, std::string access_modifiers = "")const override;
-};
-
-class CSharpClass: public AbstractClassUnit
-{
-public:
-    const std::vector< std::string > ACCESS_MODIFIERS =
-        { "public", "protected", "private", "internal", "protected_internal", "private_protected", "file"};
-    CSharpClass(const std::string& name):AbstractClassUnit(name)
-    {
-        m_fields.resize(ACCESS_MODIFIERS.size());
-    }
-    void add(const std::shared_ptr< Unit >& unit, Flags flags) override;
-    std::string compile( unsigned int level = 0, std::string access_modifiers = "") const override;
-};
-
-
 #endif // CLASSUNIT_H
